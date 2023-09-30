8c801573f8369e8157bdb31caf8fd87a179047b2
pstnv <pstnv@mail.ru>
2023-09-30 15:23:28 +0500

    created unordered list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: 1,
+    title: 'Read textbook'
+  },
+  {
+    id: 2,
+    title: 'Watch videos'
+  },
+  {
+    id: 3,
+    title: 'Complete assignment'
+  }
+];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Todo List</h1>
+      <ul>
+        { todoList.map(({ id, title }) => <li key={ id }> { title } </li>) }
+      </ul>
     </div>
   );
 }
