92f717a3d4c50eaa4c2dd6be1c3d8901cffe9c99
pstnv <pstnv@mail.ru>
2023-10-25 23:12:00 +0500

    Move Todo List into State

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
-  const [newTodo, setNewTodo] = React.useState('');
+  const [newTodo, setNewTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
 
   return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>Todo List</h1>
-      <AddTodoForm onAddTodo={ setNewTodo } />
-      <p>{ newTodo }</p>
-      <TodoList />
-    </div>
+      <div>
+          <h1 style={{ textAlign: "center" }}>Todo List</h1>
+          <AddTodoForm onAddTodo={setNewTodo} />
+          <p>{newTodo}</p>
+      <TodoList todoList={todoList} />
+      </div>
   );
 }
 
