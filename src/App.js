d7310f52a934aed48d4c5c75a1e044d43c538f5f
pstnv <pstnv@mail.ru>
2023-10-25 23:27:33 +0500

    Add New Todo to List

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
-  const [newTodo, setNewTodo] = useState('');
   const [todoList, setTodoList] = useState([]);
-
+  function addTodo(newTodo) {
+    setTodoList([...todoList, newTodo]);
+  }
   return (
       <div>
           <h1 style={{ textAlign: "center" }}>Todo List</h1>
-          <AddTodoForm onAddTodo={setNewTodo} />
-          <p>{newTodo}</p>
-      <TodoList todoList={todoList} />
+          <AddTodoForm onAddTodo={addTodo} />
+          <TodoList todoList={todoList} />
       </div>
   );
 }
