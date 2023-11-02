423c2f2bd6bd1f40ef8fc100c4cebc5924cc14ee
pstnv <pstnv@mail.ru>
2023-11-02 22:26:22 +0500

    save data in localStorage with useEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import { useState } from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
+import React from "react";
+import { useState, useEffect } from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
+const TODOLIST_KEY = "savedTodoList";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  function addTodo(newTodo) {
-    setTodoList([...todoList, newTodo]);
-  }
-  return (
-      <div>
-          <h1 style={{ textAlign: "center" }}>Todo List</h1>
-          <AddTodoForm onAddTodo={addTodo} />
-          <TodoList todoList={todoList} />
-      </div>
-  );
+    const savedTodoList = JSON.parse(localStorage.getItem(TODOLIST_KEY)) || [];
+    const [todoList, setTodoList] = useState(savedTodoList);
+
+    function addTodo(newTodo) {
+        setTodoList([...todoList, newTodo]);
+    }
+    useEffect(() => {
+        const todoListJSON = JSON.stringify(todoList);
+        localStorage.setItem(TODOLIST_KEY, todoListJSON);
+    }, [todoList]);
+
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>Todo List</h1>
+            <AddTodoForm onAddTodo={addTodo} />
+            <TodoList todoList={todoList} />
+        </div>
+    );
 }
 
 export default App;
