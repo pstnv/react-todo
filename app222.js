e8a90074d5d263d6cbab9b9ef1c0a35245757d04
pstnv <pstnv@mail.ru>
2023-11-05 15:14:23 +0500

    Refactoring code: make separate function fetchData to call it twice and not repeat the same code

---
+++ app222.js
@@ -0,0 +1,206 @@
+// Custom Http Module
+function customHttp() {
+  return {
+    get(url, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        xhr.send();
+      } catch (error) {
+        cb(error);
+      }
+    },
+    post(url, body, headers, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        if (headers) {
+          Object.entries(headers).forEach(([key, value]) => {
+            xhr.setRequestHeader(key, value);
+          });
+        }
+
+        xhr.send(JSON.stringify(body));
+      } catch (error) {
+        cb(error);
+      }
+    },
+  };
+}
+// Init http module
+const http = customHttp();
+
+const newsService = (function() {
+  const apiKey = '7fe5d05bba4f41e3924a10ce56746a46';
+  const apiUrl = 'https://newsapi.org/v2';
+
+  return {
+    topHeadlines(country = 'ru', category='general', cb) {
+      http.get(
+        `${apiUrl}/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`,
+        cb,
+      );
+    },
+    everything(query, cb) {
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
+    },
+  };
+})();
+
+// Elements
+const form = document.forms['newsControls'];
+const countrySelect = form.elements['country'];
+const categorySelect = form.elements['category'];
+const searchInput = form.elements['search'];
+const newsContainer = document.querySelector('.news-container .row');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  loadNews();
+});
+
+// form.addEventListener('change', e => {
+//   e.preventDefault();
+//   loadNews();
+// });
+
+//  init selects
+document.addEventListener('DOMContentLoaded', function() {
+  M.AutoInit();
+  loadNews();
+});
+
+// Load news function
+function loadNews() {
+  showLoader();
+
+  const country = countrySelect.value;
+  const category = categorySelect.value;
+  const searchText = searchInput.value;
+
+  if (!searchText) {
+    newsService.topHeadlines(country, category, onGetResponse);
+  } else {
+    newsService.everything(searchText, onGetResponse);
+  }
+}
+
+// Function on get response from server
+function onGetResponse(err, res) {
+  removePreloader();
+
+  if (err) {
+    onNoResults(newsContainer, err, 'error-msg');
+    return;
+  }
+
+  if (!res.articles.length) {
+    onNoResults(newsContainer, "No results.", 'error-msg');
+    return;
+  }
+
+  renderNews(res.articles);
+}
+
+// Function render news
+function renderNews(news) {
+  if (newsContainer.children.length) {
+    clearContainer(newsContainer);
+  }
+  let fragment = '';
+
+  news.forEach(newsItem => {
+    const el = newsTemplate(newsItem);
+    fragment += el;
+  });
+
+  newsContainer.insertAdjacentHTML('afterbegin', fragment);
+}
+
+// Function clear container
+function clearContainer(container) {
+  // container.innerHTML = '';
+  let child = container.lastElementChild;
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+}
+
+// News item template function
+function newsTemplate({ urlToImage, title, url, description }) {
+  return `
+    <div class="col s12 l6">
+      <div class="card">
+        <div class="card-image">
+          <img src="${urlToImage || 'https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'}">
+          <span class="card-title">${title || ''}</span>
+        </div>
+        <div class="card-content">
+          <p>${description || ''}</p>
+        </div>
+        <div class="card-action">
+          <a href="${url}" target="_blank">Read more</a>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+// Show alert
+function showAlert(msg, type = 'success') {
+  M.toast({ html: msg, classes: type });
+}
+
+// Function no results found
+function onNoResults(container, msg, type = 'error-msg') {
+  clearContainer(container);
+  showAlert(msg, type);
+  return;
+}
+
+//  Show loader function
+function showLoader() {
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <div class="progress">
+      <div class="indeterminate"></div>
+    </div>
+  `,
+  );
+}
+
+// Remove loader function
+function removePreloader() {
+  const loader = document.querySelector('.progress');
+  if (loader) {
+    loader.remove();
+  }
+}
