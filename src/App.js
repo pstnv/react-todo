9529724311ec3f845507d9d9ace8198ab299b1ae
pstnv <pstnv@mail.ru>
2023-11-05 14:33:00 +0500

    Refactoring (clean out code using local storage)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from "react";
 import { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
-const TODOLIST_KEY = "savedTodoList";
 
 function App() {
-    const savedTodoList = JSON.parse(localStorage.getItem(TODOLIST_KEY)) || [];
     const [todoList, setTodoList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
@@ -40,17 +38,9 @@ function App() {
         fetchData();
     }, []);
 
-    useEffect(() => {
-        if (isLoading) {
-            return;
-        }
-        const todoListJSON = JSON.stringify(todoList);
-        localStorage.setItem(TODOLIST_KEY, todoListJSON);
-    }, [todoList]);
-
-    function addTodo(newTodo) {
-        setTodoList([...todoList, newTodo]);
-    }
+function addTodo(newTodo) {
+    setTodoList([...todoList, newTodo]);
+}
     function removeTodo(id) {
         setTodoList(todoList.filter((todo) => todo.id !== id));
     }
