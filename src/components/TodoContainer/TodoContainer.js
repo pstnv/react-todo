57ca5583af218d3b052d2af411c62cb5dba8c604
pstnv <pstnv@mail.ru>
2024-01-07 16:37:49 +0500

    create buttons to sort order - ascending, descending, newest, oldest

--- src/components/TodoContainer/TodoContainer.js
+++ src/components/TodoContainer/TodoContainer.js
@@ -2,12 +2,66 @@ import { useState, useEffect } from "react";
 import style from "./TodoContainer.module.css";
 import TodoList from "../TodoList/TodoList";
 import AddTodoForm from "../AddTodoForm/AddTodoForm";
+import SortOptionsList from "../SortOptionsList/SortOptionsList";
 const urlAPI = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
 const tokenAPI = process.env.REACT_APP_AIRTABLE_API_TOKEN;
+const sortOptions = [
+    { name: "Newest", option: "new" },
+    { name: "Oldest", option: "old" },
+    { name: "A to Z", option: "asc" },
+    { name: "Z to A", option: "desc" },
+];
+const sortList = {
+    asc(list) {
+        list.sort((objectA, objectB) => {
+            if (objectA.title > objectB.title) {
+                return 1;
+            } else if (objectA.title === objectB.title) {
+                return 0;
+            } else {
+                return -1;
+            }
+        });
+    },
+    desc(list) {
+        list.sort((objectA, objectB) => {
+            if (objectA.title > objectB.title) {
+                return -1;
+            } else if (objectA.title === objectB.title) {
+                return 0;
+            } else {
+                return 1;
+            }
+        });
+    },
+    new(list) {
+        list.sort((objectA, objectB) => {
+            if (objectA.createdTime < objectB.createdTime) {
+                return 1;
+            }
+            if (objectA.createdTime > objectB.createdTime) {
+                return -1;
+            }
+            return 0;
+        });
+    },
+    old(list) {
+        list.sort((objectA, objectB) => {
+            if (objectA.createdTime < objectB.createdTime) {
+                return -1;
+            }
+            if (objectA.createdTime > objectB.createdTime) {
+                return 1;
+            }
+            return 0;
+        });
+    },
+};
 
 function TodoContainer() {
     const [todoList, setTodoList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [sortOption, setSortOption] = useState("new");
 
     const fetchData = async (params, id = "") => {
         const options = {
@@ -17,7 +71,7 @@ function TodoContainer() {
                 Authorization: `Bearer ${tokenAPI}`,
             },
         };
-        const url = id ? `${urlAPI}/${id}` : urlAPI;
+        const url = `${urlAPI}/${id}`;
         try {
             const response = await fetch(url, options);
             if (!response.ok) {
@@ -36,27 +90,30 @@ function TodoContainer() {
                 method: "GET",
             };
             const data = await fetchData(params);
-            const todos = data.records
-                .map((record) => {
-                    return {
-                        id: record.id,
-                        title: record.fields.title,
-                    };
-                })
-                .sort((objectA, objectB) => {
-                    if (objectA.title > objectB.title) {
-                        return 1;
-                    } else if (objectA.title === objectB.title) {
-                        return 0;
-                    } else {
-                        return -1;
-                    }
-                });
+            const todos = data.records.map((record) => {
+                return {
+                    id: record.id,
+                    title: record.fields.title,
+                    createdTime: record.createdTime,
+                };
+            });
+            if (sortOption) {
+                sortList[sortOption](todos);
+            }
             setTodoList(todos);
             setIsLoading(false);
         };
         fetchTodos();
     }, []);
+
+    useEffect(() => {
+        const todos = [...todoList];
+        if (!todos.length || !sortOption) {
+            return;
+        }
+        sortList[sortOption](todos);
+        setTodoList(todos);
+    }, [sortOption, todoList.length]);
 
     const addTodo = async (title) => {
         const todo = {
@@ -72,6 +129,7 @@ function TodoContainer() {
         const newTodo = {
             id: data.id,
             title: data.fields.title,
+            createdTime: data.createdTime,
         };
         setTodoList([...todoList, newTodo]);
     };
@@ -83,10 +141,21 @@ function TodoContainer() {
         fetchData(params, id);
         setTodoList(todoList.filter((todo) => todo.id !== id));
     }
+
+    function sortTodoList(option) {
+        setSortOption(option);
+    }
+
     return (
         <div className={style.container}>
             <h1 className={style.header}>Todo List</h1>
             <AddTodoForm onAddTodo={addTodo} />
+            {!isLoading && (
+                <SortOptionsList
+                    sortOptions={sortOptions}
+                    onSortTodoList={sortTodoList}
+                />
+            )}
             {isLoading ? (
                 <p className={style.loading}>Loading...</p>
             ) : (
