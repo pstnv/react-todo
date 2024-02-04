6ebb20c107efd7653348cc6729a9e226339781bf
pstnv <pstnv@mail.ru>
2024-02-04 18:19:17 +0500

    refactor: use reusable component Button for buttons in ListsItem

--- src/components/ListsItem/ListsItem.js
+++ src/components/ListsItem/ListsItem.js
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
-import style from './ListsItem.module.css';
+import Button from "../Button/Button";
+import style from "./ListsItem.module.css";
+import iconEdit from "../../assets/icons/editItem.png";
+import iconDelete from "../../assets/icons/deleteList.png";
 
 function ListsItem({ id, name, onRenameList, onDeleteList }) {
-    const onClickHandler = () => {
+    const onClickEditHandler = () => {
         const newTitle = prompt("Enter new name", name);
         if (!newTitle) {
             return;
@@ -14,8 +17,16 @@ function ListsItem({ id, name, onRenameList, onDeleteList }) {
             <Link to={`/list/${id}`} state={{ id, name }}>
                 {name}
             </Link>
-            <button onClick={onClickHandler}>Rename list</button>
-            <button onClick={() => onDeleteList(id, name)}>Delete list</button>
+            <Button styles={style.button} onClickHandler={onClickEditHandler}>
+                <img src={iconEdit} alt="edit todo" />
+            </Button>
+            <Button
+                styles={style.button}
+                onClickHandler={onDeleteList}
+                params={[id, name]}
+            >
+                <img src={iconDelete} alt="remove todo" />
+            </Button>
         </li>
     );
 }
