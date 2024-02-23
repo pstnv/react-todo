a6f55a40e4c4f1a4956ccaab297418527fa81ee2
pstnv <pstnv@mail.ru>
2024-02-23 13:20:01 +0500

    refactor: add focusing on input in InputWithLabel Component

--- src/components/UI/InputWithLabel/InputWithLabel.js
+++ src/components/UI/InputWithLabel/InputWithLabel.js
@@ -1,17 +1,18 @@
 import PropTypes from "prop-types";
 import style from "./InputWithLabel.module.css";
+import { useEffect, useRef } from "react";
 
-function InputWithLabel({
-    children,
-    type,
-    id,
-    todoTitle,
-    handleTitleChange,
-}) {
+function InputWithLabel({ children, type, id, todoTitle, handleTitleChange }) {
+    const inputRef = useRef();
+    useEffect(() => {
+        inputRef.current.focus();
+    });
+
     return (
         <>
             <label htmlFor={id}>{children}</label>
             <input
+                ref={inputRef}
                 type={type}
                 id={id}
                 value={todoTitle}
